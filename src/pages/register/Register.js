00f71f6f453753a471ac65cae7c8ca2fbe12236b
HARSH SHARMA <99663398+Harsh70830@users.noreply.github.com>
2023-01-06 08:05:36 +0530

    Update Register.js

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -22,7 +22,7 @@ export default function Register() {
       };
       try {
         await axios.post("/auth/register", user);
-        history.push("/login");
+        history.push("/auth/login");
       } catch (err) {
         console.log(err);
       }
