24ea5ff3887ce2e0ddbc79ab5991ff7651cad7b0
HARSH SHARMA <99663398+Harsh70830@users.noreply.github.com>
2023-01-06 08:42:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,18 +16,16 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Router exact path="/">
-        <Home/>
-        </Router>
-        <Router path="/login">
-        <Login/>
-        </Router>
-        <Router path="/register">
-        <Register/>
-        </Router>
-        <Router path="/profile/:username">
-        <Profile/>
-        </Router>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
       </Switch>
     </Router>
   );
